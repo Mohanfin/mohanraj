a9c0c1e5c427e45f9528ca19eab4d3ae52ea5f83
Mohanfin <mohamohan03@gmail.com>
2026-06-15 17:25:03 +0530

    Add files via upload

---
+++ mode.py
@@ -0,0 +1,11 @@
+# 2.starts mode
+"""import statistics
+data=[1,2,2,3,4,5,5]
+result=statistics.mode(data)
+print("mode=",result.mode[0])
+print("count=",result.mode[0])"""
+#3.pandas
+import pandas as pd
+s=pd.series([10,20,30,40,50])
+print(s)
+
